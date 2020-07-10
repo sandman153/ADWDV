14994e7426be48cdd3aafd6fdada2c1bc690bd3f
Sarath Boppudi <26595971+sandman153@users.noreply.github.com>
2020-07-10 17:26:14 +1000

    add yml

--- Adventure Works Data Vault/AdventureWorksDV/Tables/HUB/EmailAddressHUB.sql
+++ Adventure Works Data Vault/AdventureWorksDV/Tables/HUB/EmailAddressHUB.sql
@@ -2,12 +2,12 @@
 	-- Email Address VID
 	EmailAddressVID                         NVARCHAR(512) NOT NULL,
 	-- Email Address is used by Person that is a kind of Business Entity that has Business Entity ID
-	PersonID                                BIGINT NOT NULL,
+	BusinessEntityID                                BIGINT NOT NULL,
 	-- Email Address has Email Address ID
 	EmailAddressID                          BIGINT NOT NULL,
 	-- Primary index to Email Address HUB
 	PRIMARY KEY CLUSTERED(EmailAddressVID),
 	-- Unique index to Email Address HUB over PresenceConstraint over (Person, Email Address ID in "Email Address is used by Person", "Email Address has Email Address ID") occurs at most one time
-	UNIQUE NONCLUSTERED(PersonID, EmailAddressID)
+	UNIQUE NONCLUSTERED(BusinessEntityID, EmailAddressID)
 )
 GO
